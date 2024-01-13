76e7d5bd0c996f1e6d433ddbb62b9a5b897d4e19
Gordon Bosley <dowspirit@gmail.com>
2024-01-13 06:42:59 -0600

    Adding Global Var

---
+++ global_var_function/global-func.py
@@ -0,0 +1,15 @@
+global_var = 10
+
+# variables inside functions search inside before going outside.
+# by creating a local variable inside the function tied to the global
+# increases the performance +10%
+
+def func():
+    ans = 0
+    local_var = global_var  # include this before the for loop
+    for i in range(1000):
+        ans += global_var * i
+    return ans
+    
+func()
+print("Function is completed")
